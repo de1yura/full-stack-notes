c067218f4f49c00448e8464167e2f55b4722ceb9
Sajed Azizi <sajed@sajeds-air.lan>
2021-09-30 20:51:27 +0100

    list added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,6 +23,7 @@ const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
+    marginBottom: 100,
   },
   appBar: {
     transition: theme.transitions.create(['margin', 'width'], {
